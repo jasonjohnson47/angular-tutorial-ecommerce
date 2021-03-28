dada851b26afadf125c6fe74e5994e8d741d4920
jasonjohnson47 <jasonjohnson47@gmail.com>
2021-03-28 18:03:43 -0400

    Initial commit

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
+import { CartService } from '../cart.service';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
+
+  product!: Product;
+
+  ngOnInit() {
+    // First get the product id from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that corresponds with the id provided in route
+    const selectedProduct = products.find(product => product.id === productIdFromRoute)
+    if (selectedProduct) {
+      this.product = selectedProduct;
+    }
+    
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
+}
